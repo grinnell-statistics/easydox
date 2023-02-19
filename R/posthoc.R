24d81f6dd6588e9e1e23c3570ce4f2b2829ef14e
Huandong Chang <huandongchang@hotmail.com>
2023-02-19 16:30:54 -0600

    add dox_aov

---
+++ R/posthoc.R
@@ -0,0 +1,27 @@
+#' An ANOVA summary table with total DF & SS
+#'
+#' This function gives an ANOVA summary table with total degrees of freedom and sum of squares
+#' @param aov the anova model
+#' @return a html table
+#' @importFrom dplyr %>%
+#' @importFrom kableExtra kable kable_styling
+#' @export
+dox_aov=function(aov){
+  if(length(summary(aov)) > 1){
+    stop("This function only works for ANOVA with one summary table. Designs like split-plot do not work.")
+  }
+
+
+  table <- summary(aov)[[1]]
+  lastest_rownames = c(rownames(table),"Total")
+
+
+  # Add extra row for sum of DF and SS
+  extra_row <- c(sum(table$Df), sum(table[["Sum Sq"]]),NA,NA,NA)
+  anova_results <- rbind(table, extra_row)
+  rownames(anova_results) <- lastest_rownames
+
+  # Create ANOVA summary table with kable
+  kable(format(anova_results, digits = 4), align = 'c',
+        caption = "ANOVA Summary", escape = F, format.args = list(big.mark = ","))  %>% kable_styling()
+}
